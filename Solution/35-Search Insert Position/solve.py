6102914cf93e6b36ae56807f74d8af2e80f4fa15
Jackyzaz <soravit.sukkarn@gmail.com>
2024-06-24 20:29:05 +0700

    [+] add | 35-Search Insert Position

---
+++ Solution/35-Search Insert Position/solve.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            if nums[i] > target:
+                return i
+        
+        return len(nums)
+
+
+    
+
+
+print(Solution().searchInsert(nums=[1,3,5,6],target=7))
